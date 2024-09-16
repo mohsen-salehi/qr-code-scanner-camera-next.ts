cdc80009e50379ab0e825c2d17c69d35048ea5f1
mohsen.salehi <mohsensalehi2001@gmail.com>
2024-09-16 09:33:17 +0330

    add qr-code

---
+++ components/qr-code.tsx
@@ -0,0 +1,42 @@
+"use client";
+import { useState } from "react";
+import { QrReader } from "react-qr-reader";
+
+const QrCodeScanner = () => {
+  const [scanResult, setScanResult] = useState<string | null>(null);
+
+  const handleScan = (data: string | null) => {
+    if (data) {
+      setScanResult(data); // قرار دادن نتیجه در اینپوت
+    }
+  };
+
+  const handleError = (err: any) => {
+    console.error(err);
+  };
+
+  return (
+    <div className="w-full flex flex-wrap items-center justify-center">
+      <QrReader
+        onResult={(result, error) => {
+          if (result) {
+            handleScan(result.getText());
+          }
+          if (error) {
+            handleError(error);
+          }
+        }}
+        constraints={{ facingMode: "environment" }} // به صورت پیش‌فرض دوربین پشت
+        style={{ width: "100%" }}
+      />
+      <input
+        type="text"
+        value={scanResult || ""}
+        readOnly
+        placeholder="Scan result"
+      />
+    </div>
+  );
+};
+
+export default QrCodeScanner;
